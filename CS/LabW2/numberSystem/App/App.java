94c9d3c699f8c72d5d063cbeaea04ced7a8e079f
spx <elio.interesante@gmail.com>
2022-08-11 18:33:41 +0700

    Made Decimal-to-base{2,8} successfully

---
+++ CS/LabW2/numberSystem/App/App.java
@@ -0,0 +1,15 @@
+package CS.LabW2.numberSystem.App;
+
+import CS.LabW2.numberSystem.App.Converters.*;
+
+public class App {
+    public static void main(String[] args) {
+        // ! Normal style initiation;
+        // Dec2Bin dec2bin_converter = new Dec2Bin();
+        // dec2bin_converter.toBinary(348);
+    
+        // ! Static style (More fast, no need multiple classes)
+        // Dec2AnyBase.toNewBaseNumber(378)
+        System.out.println(Dec2AnyBase.toNewBaseNumber(378, 2));
+    } 
+}
---
+++ CS/LabW2/numberSystem/Playground/dec2bin.java
@@ -0,0 +1,13 @@
+package CS.LabW2.numberSystem.Playground;
+
+import java.util.Scanner;
+
+public class dec2bin {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your decimal number : ");
+        int dec = Integer.parseInt(sc.nextLine());
+
+        System.out.println(dec);
+    }    
+}
