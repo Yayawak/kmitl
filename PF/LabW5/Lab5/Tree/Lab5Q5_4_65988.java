14594da1d46021f16b04100cc3b0af0aff4cc20f
avondale <elio.interesante@gmail.com>
2022-09-13 00:15:06 +0700

    normal pushing

--- PF/LabW5/Lab5/Tree/Lab5Q5_4_65988.java
+++ PF/LabW5/Lab5/Tree/Lab5Q5_4_65988.java
@@ -1,30 +1,34 @@
 package PF.LabW5.Lab5.Tree;
 import java.util.Scanner;
 
-public class completeMultiTree {
+public class Lab5Q5_4_65988 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int i = 0;
         while (true) {
-            System.out.print("Enter Number of sub-tree : ");
+            System.out.print("\033[1;33mEnter Number of sub-tree : ");
             int height_each_sub_tree = sc.nextInt();
 
-            System.out.print("Enter Height of each sub-tree :");
+            System.out.print("\033[1;34mEnter Height of each sub-tree :");
             int n_sub_tree = sc.nextInt();
 
             // drawTree(5, 4);
             drawTree(height_each_sub_tree, n_sub_tree);
             i++;
             if(i==4) break;
+
+            for (int breakLine=0; breakLine<5; breakLine++) {
+                System.out.println();
+            }
         }
         sc.close();
     }
     static void drawTree(int height_each_sub_tree, int n_sub_tree) {
         int n_row = n_sub_tree * height_each_sub_tree;
-        int n_col = (int)(1.25*n_row);
+        int n_col = (int)(1.15*n_row);
 
         System.out.println();
-        System.out.println("Height of tree : " + n_row);
+        System.out.println("\033[1;31mHeight of tree : " + n_row);
         System.out.println("Width of tree : " + n_col);
         System.out.println();
 
@@ -67,7 +71,7 @@ public class completeMultiTree {
             System.out.print(" ");
         }
         for(int col=0; col<n_exist_star; col++) {
-            System.out.print("X");
+            System.out.print("\033[0;32mX");
         }
         for(int col=0; col<n_half_empty; col++) {
             System.out.print(" ");
